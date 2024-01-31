ac244439c3a896d3f47bc189c756902afa70b5f9
БахытжановА <@206-4>
2024-01-31 21:22:54 +0600

    Добавьте файлы проекта.

---
+++ MySEP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MySEP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
