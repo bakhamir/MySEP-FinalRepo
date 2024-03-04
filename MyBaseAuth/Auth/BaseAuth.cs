9d336c2b1d56df82dee36ace9cc122d0f228a896
Amir <Amir@WIN-S7KG9HKT0QA>
2024-03-04 22:51:04 +0600

    работа на последнем уроке

---
+++ MyBaseAuth/Auth/BaseAuth.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Text;
+
+namespace MyBaseAuth.Auth
+{
+    public class BasicAuth : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuth(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("No section Authorization");
+            try
+            {
+                var value = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credValue = Convert.FromBase64String(value.Parameter);
+                var credArray = Encoding.UTF8.GetString(credValue).Split(':');
+                var login = credArray[0];
+                var psw = credArray[1];
+
+                // Данные из базы данных
+                string dbCred = "user1:1234";
+                var dbCheckArray = dbCred.Split(':');
+
+                // Проверка соответствия логина и пароля
+                if (login == dbCheckArray[0] && psw == dbCheckArray[1])
+                {
+                    var claims = new[]
+                    {
+                    new Claim(ClaimTypes.Name, login),
+                    new Claim("psw", psw) 
+                    };
+
+                    var identity = new ClaimsIdentity(claims, Scheme.Name);
+                    var principal = new ClaimsPrincipal(identity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
+                    return AuthenticateResult.Success(ticket);
+                }
+                else
+                {
+                    return AuthenticateResult.Fail("Invalid err");
+                }
+            }
+            catch (Exception err)
+            {
+                return AuthenticateResult.Fail(err.Message);
+            }
+        }
+    }
+}
---
+++ MyBaseAuth/Controllers/Test.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text;
+
+namespace MyBaseAuth.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+
+    public class Test : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("SayHello")]
+        public ActionResult SayHello(string name)
+        {
+            return Ok($"Hello {name} {User.Identity.Name} {User.FindFirstValue("psw")}");
+            //yopta debug 
+        }
+        [Authorize]
+        [HttpGet("Download")]
+        public ActionResult Download(string name)
+        {
+            string fileContent = $"Hello {name} {User.Identity.Name} {User.FindFirstValue("psw")}";
+             
+            byte[] byteArray = Encoding.UTF8.GetBytes(fileContent);
+             
+            string type = "text/plain";
+             
+            string fileName = "Task.txt";
+            Response.Headers.Add("Content-Disposition", $"attachment; filename={fileName}");
+            return File(byteArray, type, fileName);
+
+        }
+    }
+}
