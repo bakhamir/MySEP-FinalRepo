7a78929c61cc25bd4dd9cdefc0796c11af733222
Amir <Amir@WIN-S7KG9HKT0QA>
2024-03-05 22:58:40 +0600

    DZ про валидатьоры

---
+++ Validate/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Validate.Models;
+using System.Data.SqlClient;
+using Dapper;
+namespace Validate.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private readonly string _connectionString;
+
+        public UserController(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public bool AddUser(User user)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                // Проверка уникальности логина
+                int existingCount = connection.QuerySingleOrDefault<int>(
+                    "SELECT COUNT(*) FROM Users WHERE Login = @Login", new { user.Login });
+
+                if (existingCount > 0)
+                {
+                    return false; // Логин уже занят
+                }
+
+                // Добавление нового пользователя
+                connection.Execute(
+                    "INSERT INTO Users (Login, Password, Email, DateTime) VALUES (@Login, @Password, @Email, @DateTime)",
+                    new
+                    {
+                        user.Login,
+                        user.Password,
+                        user.Email,
+                        user.DateTime
+                    });
+            }
+
+            return true; // Пользователь успешно добавлен
+        }
+  
+    }
+}
---
+++ Validate/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Validate.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Логин обязателен для заполнения")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Пароль обязателен для заполнения")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Email обязателен для заполнения")]
+        [EmailAddress(ErrorMessage = "Некорректный формат email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Дата и время обязательны для заполнения")]
+        public DateTime DateTime { get; set; }
+    }
+}
