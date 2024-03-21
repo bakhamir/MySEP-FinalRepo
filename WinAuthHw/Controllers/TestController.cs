9bc7b1128862124e2b4d3f951e787382598e28fe
Amir <Amir@WIN-S7KG9HKT0QA>
2024-03-21 16:23:42 +0500

    WINAUTHHW

---
+++ WinAuthHw/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WinAuthHw.Controllers
+{
+    [Authorize]
+    public class TestController : Controller
+    {
+        [Authorize(Users = "user*")]
+        public ActionResult Index1()
+        {
+            return Content("This is Index1 method. You are allowed to access because your username starts with 'user'.");
+        }
+
+        [Authorize(Users = "admin*")]
+        public ActionResult Index2()
+        {
+            return Content("This is Index2 method. You are allowed to access because your username starts with 'admin'.");
+        }
+    }
+}
